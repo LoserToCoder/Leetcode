b112bfc7a6912504db159f48d37543b10d28f9e2
liming <1474611571@qq.com>
2022-07-09 15:34:14 +0800

    dynamic p

--- src/com/leetcode/array/MaxSubArrayLen.java
+++ src/com/leetcode/array/MaxSubArrayLen.java
@@ -35,7 +35,7 @@ public class MaxSubArrayLen {
      *
      * 进阶:
      * 你能使时间复杂度在 O(n) 内完成此题吗?
-     * 链接:🔗https://leetcode-cn.com/problems/maximum-size-subarray-sum-equals-k/
+     * 链接:https://leetcode-cn.com/problems/maximum-size-subarray-sum-equals-k/
      * @param nums
      * @param k
      * @return
---
+++ src/com/leetcode/dp/LongestFibSubSeq.java
@@ -0,0 +1,62 @@
+package com.leetcode.dp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestFibSubSeq {
+
+    /**
+     * 如果序列X_1, X_2, ..., X_n满足下列条件，就说它是斐波那契式的：
+     * n >= 3
+     * 对于所有i + 2 <= n，都有X_i + X_{i+1} = X_{i+2}
+     * 给定一个严格递增的正整数数组形成序列 arr，找到 arr中最长的斐波那契式的子序列的长度。如果一个不存在，返回0 。
+     *
+     * （回想一下，子序列是从原序列 arr中派生出来的，它从 arr中删掉任意数量的元素（也可以不删），而不改变其余元素的顺序。例如，[3, 5, 8]是[3, 4, 5, 6, 7, 8]的一个子序列）
+     *
+     * 
+     *
+     * 示例 1：
+     *
+     * 输入: arr = [1,2,3,4,5,6,7,8]
+     * 输出: 5
+     * 解释: 最长的斐波那契式子序列为 [1,2,3,5,8] 。
+     * 示例2：
+     *
+     * 输入: arr = [1,3,7,11,12,14,18]
+     * 输出: 3
+     * 解释: 最长的斐波那契式子序列有 [1,11,12]、[3,11,14] 以及 [7,11,18] 。
+     * 
+     *
+     * 提示：
+     *
+     * 3 <= arr.length <= 1000
+     * 1 <= arr[i] < arr[i + 1] <= 10^9
+     ** 链接：https://leetcode.cn/problems/length-of-longest-fibonacci-subsequence
+     * @param arr
+     * @return
+     */
+    public int lenLongestFibSubseq(int[] arr) {
+        Map<Integer, Integer> indices = new HashMap<Integer, Integer>();
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            indices.put(arr[i], i);
+        }
+        /**
+         * dp 表示以每个下标对的元素作为最后两个数字的斐波那契子序列的最大长度。
+         * 当 i > j 时，dp[j][i] 表示以arr[j] 和arr[i] 作为最后两个数字的斐波那契子序列的最大长度。
+         * 初始时 dp 中的所有值都是 00
+         */
+        int[][] dp = new int[n][n];
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i - 1; j >= 0 && arr[j] * 2 > arr[i]; j--) {
+                int k = indices.getOrDefault(arr[i] - arr[j], -1);
+                if (k >= 0) {
+                    dp[j][i] = Math.max(dp[k][j] + 1, 3);
+                }
+                ans = Math.max(ans, dp[j][i]);
+            }
+        }
+        return ans;
+    }
+}
